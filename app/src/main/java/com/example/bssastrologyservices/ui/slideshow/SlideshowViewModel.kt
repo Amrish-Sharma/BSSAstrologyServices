0ef6a2284d56773beabb868a75a60b0546db3ba3
Amrish K. Sharma <amrishsharma95@gmail.com>
2020-01-11 21:59:52 +0530

    changes done in Palmistry/Kundli and home fragment xml

--- app/src/main/java/com/example/bssastrologyservices/ui/slideshow/SlideshowViewModel.kt
+++ app/src/main/java/com/example/bssastrologyservices/ui/slideshow/SlideshowViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.bssastrologyservices.ui.slideshow
 
+import android.widget.TextView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.bssastrologyservices.R
 
 class SlideshowViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        val kundlireadingContent = R.string.kundlireading_content
+        //value = kundlireadingContent
+        //val textView: TextView = root.findViewById(R.id.text_slideshow)
     }
     val text: LiveData<String> = _text
 }
